fe000c774a581f57b0a93a29082cd041060cbdcb
Frank Kojo Entsie <frank.entsie301@gmail.com>
2026-04-09 22:51:03 +0900

    first c program written

---
+++ cBeginners/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ int main(void)
+ {
+//  printf("hello, world\n");
+ printf("To C, or not to C: ");
+printf("that is the question.\n");
+ }
+// #include <stdio.h>
+/* Hello world*/ 
+// main()
+// {
+//     printf("hello, world \n");
+// }
